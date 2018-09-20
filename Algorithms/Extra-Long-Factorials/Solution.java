e61f9db5d493c6d0f3f8f55f36b5749686322b1c
Sam Littlefair <samuel.littlefair@student.manchester.ac.uk>
2018-09-20 17:54:30 +0100

    Add Kangaroo and Factorials solutions.

---
+++ Algorithms/Extra-Long-Factorials/Solution.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the extraLongFactorials function below.
+    static void extraLongFactorials(int n) {
+        BigInteger result = BigInteger.valueOf(n);
+        System.out.println(factorial(result, n-1));
+    }
+    static BigInteger factorial(BigInteger num, int i) {
+        if(i<=1) return num;
+        else return factorial(num.multiply(BigInteger.valueOf(i)), i-1);
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        extraLongFactorials(n);
+
+        scanner.close();
+    }
+}
