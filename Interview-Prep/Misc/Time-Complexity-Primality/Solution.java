57323a3c72b6ad5080d0d620698d4711ccd825c2
Sam Littlefair <samuel.littlefair@student.manchester.ac.uk>
2018-10-15 11:35:27 +0100

    Add time complexity primality solution in Java.

---
+++ Interview-Prep/Misc/Time-Complexity-Primality/Solution.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the primality function below.
+    static String primality(int n) {
+        if(n == 2) return "Prime";
+        if(n == 1 || (n > 2 && n % 2 == 0)) return "Not prime";
+        for(int i = 3; i < n/2; i+=2){
+            if(n % i == 0) return "Not prime";
+        }
+        return "Prime";
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int p = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int pItr = 0; pItr < p; pItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            String result = primality(n);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
